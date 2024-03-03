0cad321c9df4b47aab20eb24648ce864df9e3b4f
David L <158061850+davdlen@users.noreply.github.com>
2024-03-03 21:14:49 +0000

    Update Testing.cs

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -26,5 +26,29 @@ namespace CMP1903_A1_2324
         {
             return debugRoll.roll <= 6 && debugRoll.roll >=1; //Checking that value is less than or equal to 6 and greater than or equal to 1
         }
+        string debugOptions;
+        Console.WriteLine("                              "); //Debug menu after debug has run
+        Console.WriteLine("Select debug menu option: ");
+        Console.WriteLine("Rerun Debug - Press 1");
+        Console.WriteLine("Main Menu - Press 2");
+        Console.WriteLine("Close Game - Press 3");
+        Console.WriteLine("                              ");
+        debugOptions = Console.ReadLine();
+        switch (debugOptions)
+        {
+            case "1": //Restarts debug mode
+                enterDebug();
+                break;
+            case "2": //Back to main menu
+                Menu();
+                break;
+            case "3": //Closes application
+                closeGame();
+                break;
+            default: //In event of invalid input
+                Console.WriteLine("Invalid input.");
+                menuOption();
+                break;
+        }            
     }
 }
